f3298e13484c372ccc21b878507127be8b3c9df6
barissokat <barissokat@outlook.com>
2021-04-15 20:59:16 +0300

    Explore the Controllers and the MVC Pattern

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-})
-
-router.get('/pug', (req, res) => {
-  res.render('hello', {
-    name: typeof req.query.dog !== 'undefined' ? req.query.dog : 'Bob',
-    age: 5,
-    title: 'I love food!'
-  });
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
