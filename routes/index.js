f1ebcb96a11a7cd939dcd5d414c3b749b1244f88
barissokat <barissokat@outlook.com>
2021-04-18 15:13:04 +0300

    Create store form using mixins

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const storeController = require('../controllers/storeController');
 
 // Do work here
 router.get('/', storeController.homePage);
+router.get('/store/create', storeController.create);
+router.post('/store', storeController.store);
 
 module.exports = router;
