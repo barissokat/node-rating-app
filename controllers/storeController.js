6cc73f426e5d3ef6cb88b94ebf2dd8c59a8724c4
barissokat <barissokat@outlook.com>
2021-04-18 10:27:06 +0300

    Explore the Middleware and Error Handling

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,12 @@
+exports.myMiddleware = (req, res, next) => {
+    req.name = 'Baris';
+    res.cookie('name', 'Hello Baris', { maxAge: 9000 });
+    if(req.name === 'Baris') {
+        throw Error('We have error');
+    }
+    next();
+}
+
 exports.homePage = (req, res) => {
     res.render('index');
 }
