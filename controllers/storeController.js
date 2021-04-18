37e9e5b4d4488593d9f8882928813347156223d5
barissokat <barissokat@outlook.com>
2021-04-18 10:46:33 +0300

    Create Store Model

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,12 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'Baris';
-    res.cookie('name', 'Hello Baris', { maxAge: 9000 });
-    if(req.name === 'Baris') {
-        throw Error('We have error');
-    }
-    next();
-}
-
 exports.homePage = (req, res) => {
     res.render('index');
 }
