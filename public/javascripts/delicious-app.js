479efa138d899290d5e4fd49d2605deeb36305d0
barissokat <barissokat@outlook.com>
2021-04-23 13:24:20 +0300

    Use Geocoding Data with Google Maps

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,8 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+import autocomplete from './modules/autocomplete';
+
+// document.querySelector == $
+autocomplete( document.querySelector('#address'), document.querySelector('#lat'), document.querySelector('#lng') );
+// autocomplete( $('#address'), $('#lat'), $('#lng') );
