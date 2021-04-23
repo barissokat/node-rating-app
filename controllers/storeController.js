49e0e2393af1c3b52c35ac55d993f357980ba41e
barissokat <barissokat@outlook.com>
2021-04-23 20:04:53 +0300

    Fix for data visualization

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -23,6 +23,8 @@ exports.edit = async (req, res) => {
 }
 
 exports.update = async (req, res) => {
+    // Set the location data to be a point
+    req.body.location.type = 'Point';
     const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, { 
         new: true, // return the new store instead of the old one
         runValidators: true
