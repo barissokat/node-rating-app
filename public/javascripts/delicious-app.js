2772eb23d2d536fa410c847c29cb5cc3a6df553e
barissokat <barissokat@outlook.com>
2021-05-02 14:32:03 +0300

    Create an Ajax Search Interface

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -2,7 +2,10 @@ import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
+import typeAhead from './modules/typeAhead';
 
 // document.querySelector == $
 autocomplete( document.querySelector('#address'), document.querySelector('#lat'), document.querySelector('#lng') );
 // autocomplete( $('#address'), $('#lat'), $('#lng') );
+
+typeAhead( $('.search') );
